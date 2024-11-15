daf9bd9c900f3b4aa0e2fe7112df9d73d8caa4b5
rajames <RobJames44035@gmail.com>
2024-11-15 00:02:38 -0500

    Add multiboot support and update build paths
    Introduced multiboot definitions and kernel entry in kernel2. Updated linker script with proper multiboot alignment. Adjusted `pom.xml` for relative toolchain paths and kernel include directory.

--- starship/kernel2/multiboot_header.asm
+++ starship/kernel2/multiboot_header.asm
@@ -1,8 +1,8 @@
 section .multiboot_header
-    align 8
+    align 4
     dd 0x1BADB002          ; Magic number
-    dd 0x0                 ; Flags
-    dd -(0x1BADB002 + 0x0) ; Checksum
+    dd 0x00                ; Flags
+    dd -(0x1BADB002 + 0x00); Checksum
 
 section .text
 extern kmain
